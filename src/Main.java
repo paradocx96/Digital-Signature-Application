69ec6a8bf0821007d4dbdd196ff86fb5ad061cd0
Navinda Chandrasiri <adilanga@gmail.com>
2022-09-15 22:40:08 +0530

    Project Updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import javax.crypto.SecretKey;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            System.out.println("NETWORKED PROGRAM");
+            System.out.println("\n@Sender");
+
+            // Generate a secret key
+            AesEncryption aes_encryption = new AesEncryption();
+            SecretKey key = aes_encryption.init();
+
+            // Getting Message from File
+            String sender_message = aes_encryption.readFile("C:\\Projects\\Java\\Digital-Signatures-Application\\src\\input.txt");
+
+            // Encrypt user message
+            String encryptedData = aes_encryption.encrypt(sender_message);
+
+            // Display the encrypted string
+            System.out.println("Encrypted Data: " + encryptedData);
+            System.out.println("\n@Receiver");
+            System.out.println("Encrypted Data: " + encryptedData);
+
+            // Decrypt the user message
+            String decryptedData = aes_encryption.decrypt(encryptedData);
+
+            // Write original message
+            aes_encryption.writeFile(decryptedData, Paths.get("C:\\Projects\\Java\\Digital-Signatures-Application\\src\\output.txt"));
+
+        } catch (Exception ignored) {
+        }
     }
 }
